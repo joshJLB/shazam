dd310af956de111e1f97fdd82aeb3bc499224706
unknown <josh@jlbworks.com>
2018-08-24 09:46:13 -0500

    Finished Contact Page

--- themes/JLBTheme/contact.php
+++ themes/JLBTheme/contact.php
@@ -12,12 +12,12 @@ get_header(); ?>
 
   <div class="contact-container">
     <div class="contact-title">
-      <h1>We want to hear from you!</h1>
+      <h1><?=get_field('title', 'option')?></h1>
     </div>
     <div class="contact-info">
       <div class="contact-links">
-        <a href="tel:<?=get_field('phone', ('option'))?>"><i class="fas fa-phone"></i></a>
-        <a href="mailto:<?=get_field('email', ('option'))?>"><i class="fas fa-envelope"></i></a>
+        <a href="tel:<?=get_field('phone', 'option')?>"><i class="fas fa-phone"></i></a>
+        <a href="mailto:<?=get_field('email', 'option')?>"><i class="fas fa-envelope"></i></a>
         <a href="#"><i class="fab fa-facebook-f"></i></a>
       </div>
       <div class="contact-image" style="background-image:url(http://localhost/wp-content/uploads/2018/08/nashville.jpg)">
